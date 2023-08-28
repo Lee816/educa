bb6035f138c7de19c61867ba54113e93afdfa447
Lee816 <lgha0816@naver.com>
2023-08-28 10:10:07 +0900

    media 설정 추가

--- educa/settings.py
+++ educa/settings.py
@@ -122,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 미디어 파일 설정
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
