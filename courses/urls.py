7e504fb6b093be41b3bfa2a6ef2ad1af63d9eab9
Lee816 <lgha0816@naver.com>
2023-08-29 13:51:00 +0900

    courses 앱 코스 모듈의 콘텐츠 url 연결

--- courses/urls.py
+++ courses/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<int:pk>/edit/', views.CourseUpdateView.as_view(), name='course_edit'),
     path('<int:pk>/delete/', views.CourseDeleteView.as_view(), name='course_delete'),
     path('<int:pk>/module/',views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('module/<int:module_id>/content/<str:model_name>/create/', views.ContentCreateUpdateView.as_view(), name='module_content_create'),
+    path('module/<int:module_id>/content/<str:model_name>/<int:id>/',views.ContentCreateUpdateView.as_view(), name='module_content_update'),
 ]
