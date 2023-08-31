8c763bb96ac690cd6d90d074be851253a8db72a5
Lee816 <lgha0816@naver.com>
2023-08-31 09:37:56 +0900

    students 앱 학생의 코스 list, detail 뷰 작성

--- students/views.py
+++ students/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CourseEnrollForm
+from courses.models import Course
 
 # Create your views here.
 
@@ -33,3 +34,30 @@ class StudentEnrollCourseView(LoginRequiredMixin, generic.FormView):
 
     def get_success_url(self):
         return reverse_lazy('student_course_detail', args=[self.course.id])
+    
+class StudentCourseListView(LoginRequiredMixin, generic.ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in = [self.request.user])
+    
+class StudentCourseDetailView(generic.DetailView):
+    model = Course
+    template_name = 'students/course/detail.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in = [self.request.user])
+
+    # module_id URL 매개변수가 주어지면 코스 모듈을 context에 설정
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+        
+        if 'module_id' in self.kwargs:
+            context['module'] = course.modules.get(id=self.kwargs['module_id'])
+        else:
+            context['module'] = course.modules.all()[0]
+        return context
