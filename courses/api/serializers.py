3d96cd44ed94062b61c88b8751423d9e12454ad2
Lee816 <lgha0816@naver.com>
2023-09-01 13:18:18 +0900

    courses 앱 api 패키지 Course 모델 시리얼라이저 생성

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Subject
+from ..models import Subject, Course
 
 # 시리얼 라이저는 Django의 Form 및 ModelForm클래스와 유사한 방식으로 정의
 # fields 속성을 설정하지 않으면 모든 필드가 포함된다.
@@ -8,3 +8,8 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id','title','slug']
+        
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ['id','subject','title','slug','overview','created','owner','modules']
