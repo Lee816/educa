d76fe0fe2ba09b799ad03ddd164182de495d4c10
Lee816 <lgha0816@naver.com>
2023-09-01 13:25:34 +0900

    courses 앱 api 패키지 Module 시리얼라이저 추가 및 코스에 모듈시리얼라이저 중첩

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from ..models import Subject, Course
+from ..models import Subject, Course, Module
 
 # 시리얼 라이저는 Django의 Form 및 ModelForm클래스와 유사한 방식으로 정의
 # fields 속성을 설정하지 않으면 모든 필드가 포함된다.
@@ -8,8 +8,18 @@ class SubjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subject
         fields = ['id','title','slug']
+
+# 모듈 모델에 대한 직렬화
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order','title','description']
         
 class CourseSerializer(serializers.ModelSerializer):
+    # 모듈 시리얼라이저를 중첩
+    # many =True 는 여러 객체를 직렬화하는 것을 나타낸다
+    # read_only = True 는 이 필드는 읽기 전용이며 객체를 생성하거나 업데이트하는데 사용되지 않아야함을 나타낸다.
+    modules = ModuleSerializer(many=True, read_only=True)
     class Meta:
         model = Course
         fields = ['id','subject','title','slug','overview','created','owner','modules']
