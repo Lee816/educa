cbd165993619a48eb9a3b22b813f548f589e1cbb
Lee816 <lgha0816@naver.com>
2023-09-01 13:36:12 +0900

    courses 앱 api 패키지 | 코스에 학생 등록 API 뷰 작성

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-from ..models import Subject
+from ..models import Subject, Course
 from .serializers import SubjectSerializer
 
 class SubjectListView(generics.ListAPIView):
@@ -12,3 +15,12 @@ class SubjectListView(generics.ListAPIView):
 class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
+    
+class CourseEnrollView(APIView):
+    # POST 동작에 대한 메서드이며 다른 HTTP 메서드를 허용하지 않는다.
+    def post(self, request, pk, format=None):
+        # pk URL 변수를 사용하여 강좌를 가져온다.
+        course = get_object_or_404(Course, pk=pk)
+        # Course 객체의 학생 등록
+        course.students.add(request.user)
+        return Response({'enrolled':True})
