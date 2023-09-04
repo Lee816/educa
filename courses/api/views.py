82b4361cf2d8ebe08690cedcb046d481008ebc7a
Lee816 <lgha0816@naver.com>
2023-09-04 11:15:39 +0900

    courses 앱 api 패키지 강좌 내용 출력

--- courses/api/views.py
+++ courses/api/views.py
@@ -7,7 +7,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 
 from ..models import Subject, Course
-from .serializers import SubjectSerializer, CourseSerializer
+from .serializers import SubjectSerializer, CourseSerializer, CourseWithContentsSerializer
+from .permissions import IsEnrolled
 
 class SubjectListView(generics.ListAPIView):
     # 객체를 검색하기 위해 사용할 기본 QuerySet
@@ -46,3 +47,7 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled':True})
+    
+    @action(detail=True,methods=['get'], serializer_class= CourseWithContentsSerializer, authentication_classes=[BasicAuthentication],permission_classes=[IsAuthenticated,IsEnrolled])
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
