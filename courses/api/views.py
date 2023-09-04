9f2c4341611c6b6bfd430397e79cff9ebfeca6b6
Lee816 <lgha0816@naver.com>
2023-09-04 10:15:09 +0900

    courses 앱 api 패키지 사용자 뷰 권한 추가

--- courses/api/views.py
+++ courses/api/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from ..models import Subject, Course
 from .serializers import SubjectSerializer
@@ -20,6 +21,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 class CourseEnrollView(APIView):
     # 인증클래스 - 사용자는 HTTP 요청의 인증 헤더에 설정된 자격 증명으로 식별
     authentication_classes = [BasicAuthentication]
+    # 익명의 사용자가 뷰에 액세스 할 수 없다.
+    permission_classes = [IsAuthenticated]
     # POST 동작에 대한 메서드이며 다른 HTTP 메서드를 허용하지 않는다.
     def post(self, request, pk, format=None):
         # pk URL 변수를 사용하여 강좌를 가져온다.
