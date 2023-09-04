f431647b77bc306f24ff043f8576228b0ee7d67c
Lee816 <lgha0816@naver.com>
2023-09-04 10:28:12 +0900

    courses 앱 api 패키지 코스등록 뷰를 뷰셋에 추가

--- courses/api/views.py
+++ courses/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
@@ -18,20 +19,30 @@ class SubjectDetailView(generics.RetrieveAPIView):
     queryset = Subject.objects.all()
     serializer_class = SubjectSerializer
     
-class CourseEnrollView(APIView):
-    # 인증클래스 - 사용자는 HTTP 요청의 인증 헤더에 설정된 자격 증명으로 식별
-    authentication_classes = [BasicAuthentication]
-    # 익명의 사용자가 뷰에 액세스 할 수 없다.
-    permission_classes = [IsAuthenticated]
-    # POST 동작에 대한 메서드이며 다른 HTTP 메서드를 허용하지 않는다.
-    def post(self, request, pk, format=None):
-        # pk URL 변수를 사용하여 강좌를 가져온다.
-        course = get_object_or_404(Course, pk=pk)
-        # Course 객체의 학생 등록
-        course.students.add(request.user)
-        return Response({'enrolled':True})
+
+# class CourseEnrollView(APIView):
+#     # 인증클래스 - 사용자는 HTTP 요청의 인증 헤더에 설정된 자격 증명으로 식별
+#     authentication_classes = [BasicAuthentication]
+#     # 익명의 사용자가 뷰에 액세스 할 수 없다.
+#     permission_classes = [IsAuthenticated]
+#     # POST 동작에 대한 메서드이며 다른 HTTP 메서드를 허용하지 않는다.
+#     def post(self, request, pk, format=None):
+#         # pk URL 변수를 사용하여 강좌를 가져온다.
+#         course = get_object_or_404(Course, pk=pk)
+#         # Course 객체의 학생 등록
+#         course.students.add(request.user)
+#         return Response({'enrolled':True})
     
 # ReadOnlyModelViewSet 는 list() 와 retrieve()를 통해 객체 목록을 가져오거나 단일 객체를 검색하는 읽기 전용 동작
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    
+    # 데코레이터를 사용하여 추가 동작을 정의 detail=True 매개변수를 사용하여 이 동작을 단일 객체에 대해 수행되는 동작임을 지정
+    # 이 동작에는 post() 메서드만 허용되며 인증 및 권한 클래스를 설정
+    @action(detail=True, methods=['post'], authentication_classes=[BasicAuthentication], permission_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        # self.get_object()를 사용하여 Course 객체를 가져온다.
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled':True})
