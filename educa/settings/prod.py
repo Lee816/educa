a4d6dbe3ee044956550618857704d5dad33c0ca5
Lee816 <lgha0816@naver.com>
2023-09-07 10:05:39 +0900

    프로덕션 환경 설정

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,1 +1,18 @@
 # 프로덕션 환경을 위한 사용자 지정 설정
+
+from .base import *
+
+DEBUG = False
+
+# 예외가 발생하면 모든 정보가 관리 설정에 나열된 사람들에게 이메일로 전송
+ADMINS = [
+    ('Lee', 'asddsa124@naver.com'),
+]
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default' : {
+        
+    }
+}
